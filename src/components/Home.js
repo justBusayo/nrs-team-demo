dc32684d15fa890eb8e24b3c9c9fb409daa7f9f5
GentleToyin <41758364+GentleToyin@users.noreply.github.com>
2021-12-19 11:45:18 +0100

    made changes to the index.css and app.css files to center the app

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 import AudioPlayer from "react-h5-audio-player";
 import userEvent from "@testing-library/user-event"
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col} from "react-bootstrap";
 
 import Menu from "./Menu.js"
 import RadioBox from "./RadioBox.js"
 import RecentStationBox from "./RecentStationBox.js"
-
+import SearchBox from "./SearchBox.js";
 const Home = () => {
     return (
         <div>
+            <Container style={{marginTop:"1.5em"}}>
+                <SearchBox></SearchBox>
+            </Container>
             {/* for popular section */}
             <Container>
                 <div className="RadioBoxHeader">
@@ -21,15 +24,28 @@ const Home = () => {
             {/* for recent stations */}
             <Container>
                 <div className="RadioBoxHeader">
-                    <span className="boxTitle">RECENT STATIONS</span>
+                    <span className="boxTitle">CATEGORIES</span>
                     <span className="more">More</span>
                 </div>
-                <Row className="rentStations">
-                    <Col> <RecentStationBox /></Col>
-                    <Col> <RecentStationBox /> </Col>
-                    <Col> <RecentStationBox /> </Col>
-                </Row>
+                {/* <Row className="rentStations">
+                    <Col> <RecentStationBox></RecentStationBox> </Col> */}
+                    <Col> <RecentStationBox></RecentStationBox> </Col>
+                    {/* <Col> <RecentStationBox></RecentStationBox> </Col>
+                </Row> */}
             </Container>
+            {/* for favorite Box */}
+            <Container style={{marginBottom:"5em"}}>
+                <div className="RadioBoxHeader">
+                    <span className="boxTitle">FAVORITES STATIONS</span>
+                    <span className="more">More</span>
+                </div>
+                <RadioBox></RadioBox>
+            </Container>
+
+            {/* Now Playing Section */}
+            {/* <div className="nowPlaying">
+                <NowPlaying></NowPlaying>
+            </div> */}
         </div>
     )
 }
